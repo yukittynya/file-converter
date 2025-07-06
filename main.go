e499dc246afeec67cedfacb95998c81b35520b50
yukii <lichtestv@gmail.com>
2025-07-07 00:28:11 +0200

    slowly enjoying go, will be nice to add to my list :3

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
-package main;
+package main
 
 import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -15,8 +16,30 @@ func main() {
 		log.Fatal("Failed creating uploads dir :/", err);
 	}
 
-	fs := http.FileServer(http.Dir("./src/"));
-	http.Handle("/", fs);
+	//fs := http.FileServer(http.Dir("./src/"));
 
+	http.HandleFunc("/", spaHandler)
 	http.ListenAndServe(":3030", nil);
+}
+
+func spaHandler(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join("./src", path);
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join("./src", "index.html"))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.ServeFile(w, r, path)
 }
